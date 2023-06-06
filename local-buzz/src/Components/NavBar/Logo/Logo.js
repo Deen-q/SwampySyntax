2722fdd696875e954b5d01b2867fa311870a128f
Mohammad Zafar <mzcoder7@gmail.com>
2023-06-06 14:35:15 +0100

    created logo component

---
+++ local-buzz/src/Components/NavBar/Logo/Logo.js
@@ -0,0 +1,17 @@
+// logo component
+// render the logo followed by Local Buzz title
+import './Logo.css';
+import LogoWhiteOutline from '../../../assets/LogoWhiteOutline.png';
+// import LogoBlackOutline from '../../../assets/LogoBlackOutline.png';
+export default function Logo() {
+	console.log('LOGO RENDERED');
+	return (
+		<div className='logo-container'>
+			<img id='logo' src={LogoWhiteOutline} alt='logo' />
+			<div className='line'></div>
+			<h1>LocalBuzz</h1>
+			{/* <img id='logo' src={LogoBlackOutline} alt='logo' />
+			<h1>| LocalBuzz</h1> */}
+		</div>
+	);
+}
