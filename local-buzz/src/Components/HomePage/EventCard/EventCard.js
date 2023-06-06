ef38ed33e2decbae619e18b6bab9ec6b9a435732
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-06-06 14:05:47 +0100

    Created basic EventCard component

---
+++ local-buzz/src/Components/HomePage/EventCard/EventCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function EventCard(){
+    
+    return(
+        <div>
+            <img alt="CardImage"></img>
+            <h1>Event Name</h1>
+            <h3>*insert date*</h3>
+            <p>*insert description*</p>
+        </div>
+    )
+}
+
+export default EventCard;
