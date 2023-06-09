f37e733f1011c24e1aa3591f032486f940e9707a
UmamahBegg <umamah.begg@gmail.com>
2023-06-09 14:29:53 +0300

    brokencode

---
+++ local-buzz/src/Components/CreateEventPage/AddEventForm/AddEventForm.js
@@ -0,0 +1,59 @@
+import { eventData } from "../../Data/EventData";
+
+
+
+export default function AddEventForm({event, setEvent}){
+    const title = [title, setTitle] = useState("");
+    const description = [description, setDescription] = useState("");
+    const date = [date, setDate] = useState("");
+    const time = [time, setTime] = useState("");
+    const location = [location, setLocation] = useState("");
+    const image = [image, setImage] = useState("");
+    // const tags = [tags, setTags] = useState("");
+    // const link = [link, setLink] = useState("");
+
+
+function handleSubmit(event){
+
+
+    const newEvent = {
+        title: title,
+        description: description,
+        date: date,
+        time: time,
+        location: location,
+        image: image,
+        // tags: tags,
+        // link: link,
+    }
+
+const updatedEventData = [...eventData];
+updatedEventData.push(newEvent);
+console.log(updatedEventData);
+}
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label for="title">Title:</label>
+                <input type="text" id="title" name="title" placeholder="Title of Event"></input>
+                <label for="description">Description:</label>
+                <input type="text" id="description" name="description" placeholder="Description of Event"></input>
+                <label for="date">Date:</label>
+                <input type="date" id="date" name="date"></input>
+                <label for="time">Time:</label>
+                <input type="time" id="time" name="time"></input>
+                <label for="location">Location:</label>
+                <input type="text" id="location" name="location" placeholder="Location of Event"></input>
+                <label for="image">Image:</label>
+                <input type="file" id="image" name="image"></input>
+                {/* <label for="tags">Tags:</label>
+                <input type="text" id="tags" name="tags" placeholder="Tags for Event"></input>
+                <label for="link">Link:</label>
+                <input type="text" id="link" name="link" placeholder="Link to Event"></input>
+                */}
+                <input type="submit" value="Submit"></input> 
+            </form>
+        </div>
+    );
+}
--- local-buzz/src/Components/CreateEventPage/CreateEventPage.js
+++ local-buzz/src/Components/CreateEventPage/CreateEventPage.js
@@ -1,5 +1,10 @@
+import AddEventForm from "./AddEventForm/AddEventForm";
+
 export default function CreateEventPage(){
     return(
+        <div>
         <h1>Create Event Page</h1>
+        <AddEventForm/>
+        </div>
     )
 }
