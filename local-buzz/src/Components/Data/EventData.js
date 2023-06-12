a5ed8bfc7319962d37d80ed545a93f2b689e87d9
hroarr28 <thcuk1@hotmail.co.uk>
2023-06-12 12:01:34 +0100

    Final changes to MVP functions

--- local-buzz/src/Components/Data/EventData.js
+++ local-buzz/src/Components/Data/EventData.js
@@ -12,6 +12,10 @@ export const eventData = [
     description: "A get together to share local beers from local brewers.",
     date: new Date(2023, 5, 15).toLocaleDateString(), // Note: month is zero-based (0-11)
     image: BeerFestival,
+    firstLineOfAddress: "42 Baker Street",
+    city: "London",
+    postcode: "NW1 6XE",
+    time: "19:00-22:00",
   },
   {
     id: uuidv4(),
@@ -20,6 +24,10 @@ export const eventData = [
       "Let's get together and read and discuss educated by Tara Westover",
     date: new Date(2023, 8, 20).toLocaleDateString(),
     image: ReadingClub,
+    firstLineOfAddress: "15 Elmwood Road",
+    city: "Manchester",
+    postcode: "M14 5PA",
+    time: "19:00-21:00",
   },
   {
     id: uuidv4(),
@@ -28,6 +36,10 @@ export const eventData = [
       "Come and enjoy the sunshine and spend some time with your neighbours",
     date: new Date(2023, 7, 19).toLocaleDateString(),
     image: AllotmentImage,
+    firstLineOfAddress: "7 Willow Lane",
+    city: "Birmingham",
+    postcode: "B12 8QF",
+    time: "10:00-12:00",
   },
   {
     id: uuidv4(),
@@ -36,5 +48,9 @@ export const eventData = [
       "Bring what's in your garage! Meet like minded car enthusiasts. No car too old or too young. I'll be bringing my Reliant Scimitar",
     date: new Date(2023, 10, 5).toLocaleDateString(),
     image: ClassicCar,
+    firstLineOfAddress: "29 Rosemary Avenue",
+    city: "Edinburgh",
+    postcode: "EH10 6TR",
+    time: "10:00-12:00",
   },
 ];
--- local-buzz/src/Components/HomePage/EventCard/EventCard.js
+++ local-buzz/src/Components/HomePage/EventCard/EventCard.js
@@ -1,44 +1,56 @@
-import React, { useState } from 'react';
-import './EventCard.css';
+import React, {useState} from "react";
+import "./EventCard.css";
 
 function EventCard(props) {
-	// Define state variable for showing event description
-	const [show, setShow] = useState({});
+  // Define state variable for showing event description
+  const [show, setShow] = useState({});
 
-	// Define function to handle click event on image
-	function handleClick(eventId) {
-		// Update the show state variable to toggle the description of the clicked event
-		setShow(prevShow => ({
-			...prevShow, // copy the previous state object
+  // Define function to handle click event on image
+  function handleClick(eventId) {
+    // Update the show state variable to toggle the description of the clicked event
+    setShow((prevShow) => ({
+      ...prevShow, // copy the previous state object
 
-			[eventId]: !prevShow[eventId], // toggle the show property of the clicked event id
-		}));
-	}
+      [eventId]: !prevShow[eventId], // toggle the show property of the clicked event id
+    }));
+  }
 
-	// Render the EventCard component
-	return (
-		<>
-			<div className='EventCardContainer'>
-				{props.filteredData.map(event => (
-					<div key={event.id}>
-						{/* add click event listener to toggle the description of the clicked event  */}
-						<img
-							className='event-img'
-							onClick={() => handleClick(event.id)}
-							alt='CardImage'
-							src={event.image}
-						/>
-						<div className='TextBorder'>
-							<h1>{event.title}</h1>
-							<h3>{event.date}</h3>
-							{/* show the description of the clicked event if the show property is true  */}
-							{show[event.id] && <p>{event.description}</p>}
-						</div>
-					</div>
-				))}
-			</div>
-		</>
-	);
+  // Render the EventCard component
+  return (
+    <>
+      <div className='EventCardContainer'>
+        {props.filteredData.map((event) => (
+          <div key={event.id}>
+            {/* add click event listener to toggle the description of the clicked event  */}
+            <img
+              className='event-img'
+              onClick={() => handleClick(event.id)}
+              alt='CardImage'
+              src={event.image}
+            />
+            <div className='TextBorder'>
+              <h1>{event.title}</h1>
+              <div className='EventDateAndCity'>
+                <h3>{event.date}</h3>
+                <h3>{event.city}</h3>
+              </div>
+              {/* show the description of the clicked event if the show property is true  */}
+              {show[event.id] && (
+                <div>
+                  <h3>{event.time}</h3>
+                  <div className='EventAddressAndPostcode'>
+                    <h3>{event.firstLineOfAddress}</h3>
+                    <h3>{event.postcode}</h3>
+                  </div>
+                  <p>{event.description}</p>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default EventCard;
