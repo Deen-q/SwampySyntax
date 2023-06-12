051d150e8e860930fd18f17d092c2f7f1818fcd4
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-06-12 14:50:27 +0100

    Lots of CSS polish

--- local-buzz/src/Components/NavBar/SearchBar/SearchBar.js
+++ local-buzz/src/Components/NavBar/SearchBar/SearchBar.js
@@ -1,6 +1,36 @@
 import './SearchBar.css';
+import { useState } from 'react';
 function SearchBar(props) {
-	// Define function to handle enter key press
+	const [show, setShow] = useState(false)
+
+	function handleShowSearch() {
+		setShow(!show)
+	}
+
+	return (
+		<div className="searchContainer">
+		<img onClick={handleShowSearch} 
+		src="https://img.icons8.com/?size=512&id=7695&format=png" 
+		alt="Search Icon"
+		className="SearchIcon"
+
+		/>
+
+		{show === true && <input
+		// alternatively: !show &&; !show means show then the useState is false
+			id='search'
+			// onKeyUp={handleEnterClick} // Commented out because it is not being used
+			type='text'
+			placeholder='Search for an event'
+			value={props.input}
+			onChange={props.handleFilteredData} // Call the handleFilteredData function passed as a prop on input change
+		/>}
+		</div>
+	);
+}
+export default SearchBar;
+
+// Define function to handle enter key press
 	// function handleEnterClick(event) {
 	// 	// Check if the pressed key is 'Enter'
 	// 	if (event.key === 'Enter') {
@@ -8,15 +38,3 @@ function SearchBar(props) {
 	// 	}
 	// }
 	// Render the SearchBar component
-	return (
-		<input
-			id='search'
-			// onKeyUp={handleEnterClick} // Commented out because it is not being used
-			type='text'
-			placeholder='Search for an event'
-			value={props.input}
-			onChange={props.handleFilteredData} // Call the handleFilteredData function passed as a prop on input change
-		/>
-	);
-}
-export default SearchBar;
