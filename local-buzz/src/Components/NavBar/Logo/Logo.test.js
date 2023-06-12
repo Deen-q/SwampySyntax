607414303c587e4c005a8d548138488358b0c56c
ChrisKnowles <christianjknowles@gmail.com>
2023-06-12 16:16:05 +0100

    tests added to app.test.js and logo.test.js

--- local-buzz/src/Components/NavBar/Logo/Logo.test.js
+++ local-buzz/src/Components/NavBar/Logo/Logo.test.js
@@ -1,3 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Logo from './Logo';
 
-
-test ("Does the logo appear?") 
+test ("Does the logo appear?", () => {
+    render(
+      <BrowserRouter>
+        <Logo/>
+      </BrowserRouter>
+    );
+    const logoElement = screen.getByText(/LocalBuzz/i);
+    expect(logoElement).toBeInTheDocument();
+});
