4c04baf034833eef0d5f50d122cb826ff8379a32
hroarr28 <thcuk1@hotmail.co.uk>
2023-06-13 12:18:14 +0100

    Changes to eventcard CSS

--- local-buzz/src/Components/HomePage/EventCard/EventCard.js
+++ local-buzz/src/Components/HomePage/EventCard/EventCard.js
@@ -1,58 +1,65 @@
-import React, { useState } from 'react';
-import './EventCard.css';
-
+import React, {useState} from "react";
+import "./EventCard.css";
+import Dots from "../../../assets/icons8-3-dots-50.png";
 function EventCard(props) {
-	// Define state variable for showing event description
-	const [show, setShow] = useState({});
+  // Define state variable for showing event description
+  const [show, setShow] = useState({});
 
-	// Define function to handle click event on image
-	function handleClick(eventId) {
-		// Update the show state variable to toggle the description of the clicked event
-		setShow(prevShow => ({
-			...prevShow, // copy the previous state object
-		// the below is an if statement that checks if the eventId is in the show object, if it is then it will return the opposite of the current value of the show property of the eventId, if it isn't then it will return true
-			[eventId]: !prevShow[eventId], // toggle the show property of the clicked event id
-		}));
-	}
+  // Define function to handle click event on image
+  function handleClick(eventId) {
+    // Update the show state variable to toggle the description of the clicked event
+    setShow((prevShow) => ({
+      ...prevShow, // copy the previous state object
+      // the below is an if statement that checks if the eventId is in the show object, if it is then it will return the opposite of the current value of the show property of the eventId, if it isn't then it will return true
+      [eventId]: !prevShow[eventId], // toggle the show property of the clicked event id
+    }));
+  }
 
-	// Render the EventCard component
-	return (
-		<>
-			<div className='EventCardContainer'>
-				{props.filteredData.map(event => (
-					<div key={event.id}>
-						{/* add click event listener to toggle the description of the clicked event  */}
-						<div className='TextBorder'>
-							<img
-								className='event-img'
-								onClick={() => handleClick(event.id)}
-								alt='CardImage'
-								src={event.image}
-							/>
+  // Render the EventCard component
+  return (
+    <>
+      <div className='EventCardContainer'>
+        {props.filteredData.map((event) => (
+          <div key={event.id}>
+            {/* add click event listener to toggle the description of the clicked event  */}
+            <div className='TextBorder'>
+              <img
+                className='event-img'
+                onClick={() => handleClick(event.id)}
+                alt='CardImage'
+                src={event.image}
+              />
 
-							<h2>{event.title}</h2>
-							<div className='EventDateAndCity'>
-								<div className='contentContainer'></div>
-								<p>{event.date}</p>
-								<p>{event.city}</p>
-							</div>
-							{/* show the description of the clicked event if the show property is true  */}
-							{show[event.id] && (
-								<div>
-									<div className='EventAddressAndPostcode'>
-										<p>{event.firstLineOfAddress}</p>
-										<p>{event.postcode}</p>
-									</div>
-									<p>{event.time}</p>
-									<p>{event.description}</p>
-								</div>
-							)}
-						</div>
-					</div>
-				))}
-			</div>
-		</>
-	);
+              <h2>{event.title}</h2>
+              <img
+                className='event-img'
+                onClick={() => handleClick(event.id)}
+                alt='CardImage'
+                src={Dots}
+              />
+              <div className='EventDateAndCity'>
+                <div className='contentContainer'></div>
+                <p>{event.date}</p>
+                {!show[event.id] && <p>{event.city}</p>}
+              </div>
+              {/* show the description of the clicked event if the show property is true  */}
+              {show[event.id] && (
+                <div>
+                  <p>{event.time}</p>
+                  <p>
+                    {event.firstLineOfAddress}, {event.city}, {event.postcode}
+                  </p>
+                  {/* <p></p> */}
+
+                  <p className='EventDescription'>{event.description}</p>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default EventCard;
