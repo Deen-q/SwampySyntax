825b35400dd6f743223dc9c6b8855f4f7892fee9
hroarr28 <thcuk1@hotmail.co.uk>
2023-06-19 17:22:41 +0100

    Logo routing to correct links

--- local-buzz/src/Components/NavBar/Logo/Logo.js
+++ local-buzz/src/Components/NavBar/Logo/Logo.js
@@ -1,21 +1,34 @@
+// We made one copy of the Logo to take us back to the Landing
 // logo component
 // render the logo followed by Local Buzz title
 import "./Logo.css";
 import LogoWhiteOutline from "../../../assets/LogoWhiteOutline.png";
 import {Link} from "react-router-dom";
+import {UserContext} from "../../../contexts/user.context";
+
+import {useContext} from "react";
+
 // import LogoBlackOutline from '../../../assets/LogoBlackOutline.png';
-export default function Logo(props) {
+export default function LogoLandingPage(props) {
+  const {user} = useContext(UserContext);
+
   return (
     <div className='logo-container'>
-      <Link to='/homepage'>
-        {" "}
-        <img id='logo' src={LogoWhiteOutline} alt='logo' />{" "}
-      </Link>
-
+      {user ? (
+        <Link to='/homepage'>
+          <img id='logo' src={LogoWhiteOutline} alt='logo' />
+        </Link>
+      ) : (
+        <Link to='/'>
+          <img id='logo' src={LogoWhiteOutline} alt='logo' />
+        </Link>
+      )}
       <div className='line'></div>
-      <Link to='/homepage'> {props.show && <h1>LocalBuzz</h1>}</Link>
-      {/* <img id='logo' src={LogoBlackOutline} alt='logo' />
-			<h1>| LocalBuzz</h1> */}
+      {user ? (
+        <Link to='/homepage'>{props.show && <h1>LocalBuzz</h1>}</Link>
+      ) : (
+        <Link to='/'>{props.show && <h1>LocalBuzz</h1>}</Link>
+      )}
     </div>
   );
 }
--- local-buzz/src/Components/NavBar/NavBar.js
+++ local-buzz/src/Components/NavBar/NavBar.js
@@ -2,7 +2,6 @@ import SearchBar from "./SearchBar/SearchBar";
 import Logo from "./Logo/Logo.js";
 import "./NavBar.css";
 import {useState} from "react";
-import LogoLandingPage from "./Logo/LogoLandingPage";
 
 export default function NavBar(props) {
   const [show, setShow] = useState(true);
@@ -13,7 +12,6 @@ export default function NavBar(props) {
 
   return (
     <div id='navbar'>
-      <LogoLandingPage show={show} />
       <Logo show={show} />
       <SearchBar
         show={show}
