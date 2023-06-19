e3e120a3c3c3b471321325c1f95bff411c8a8bba
hroarr28 <thcuk1@hotmail.co.uk>
2023-06-19 16:38:13 +0100

    Working on routers for logo-whether logged in or not

--- local-buzz/src/Components/NavBar/Logo/Logo.js
+++ local-buzz/src/Components/NavBar/Logo/Logo.js
@@ -2,18 +2,18 @@
 // render the logo followed by Local Buzz title
 import "./Logo.css";
 import LogoWhiteOutline from "../../../assets/LogoWhiteOutline.png";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 // import LogoBlackOutline from '../../../assets/LogoBlackOutline.png';
 export default function Logo(props) {
   return (
-    <div className="logo-container">
-      <Link to="/">
+    <div className='logo-container'>
+      <Link to='/homepage'>
         {" "}
-        <img id="logo" src={LogoWhiteOutline} alt="logo" />{" "}
+        <img id='logo' src={LogoWhiteOutline} alt='logo' />{" "}
       </Link>
 
-      <div className="line"></div>
-      {props.show && <h1>LocalBuzz</h1>}
+      <div className='line'></div>
+      <Link to='/homepage'> {props.show && <h1>LocalBuzz</h1>}</Link>
       {/* <img id='logo' src={LogoBlackOutline} alt='logo' />
 			<h1>| LocalBuzz</h1> */}
     </div>
--- local-buzz/src/Components/NavBar/NavBar.js
+++ local-buzz/src/Components/NavBar/NavBar.js
@@ -1,7 +1,8 @@
 import SearchBar from "./SearchBar/SearchBar";
 import Logo from "./Logo/Logo.js";
 import "./NavBar.css";
-import { useState } from "react";
+import {useState} from "react";
+import LogoLandingPage from "./Logo/LogoLandingPage";
 
 export default function NavBar(props) {
   const [show, setShow] = useState(true);
@@ -11,7 +12,8 @@ export default function NavBar(props) {
   }
 
   return (
-    <div id="navbar">
+    <div id='navbar'>
+      <LogoLandingPage show={show} />
       <Logo show={show} />
       <SearchBar
         show={show}
