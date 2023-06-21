99d75befb73b3e7ef39466f6a615e694ac9b4547
Mohammad Zafar <mzcoder7@gmail.com>
2023-06-21 11:01:22 +0100

    more styling in progress

--- local-buzz/src/Components/NavBar/NavBar.js
+++ local-buzz/src/Components/NavBar/NavBar.js
@@ -1,26 +1,29 @@
-import SearchBar from "./SearchBar/SearchBar";
-import Logo from "./Logo/Logo.js";
-import PersonIcon from "./LoginIcon/loginIcon";
-import "./NavBar.css";
-import {useState} from "react";
+import SearchBar from './SearchBar/SearchBar';
+import Logo from './Logo/Logo.js';
+import PersonIcon from './LoginIcon/loginIcon';
+import './NavBar.css';
+import { useState } from 'react';
 
 export default function NavBar(props) {
-  const [show, setShow] = useState(true);
+	const [show, setShow] = useState(true);
 
-  function handleShowSearch() {
-    setShow(!show);
-  }
+	function handleShowSearch() {
+		setShow(!show);
+	}
 
-  return (
-    <div id='navbar'>
-      <Logo show={show} />
-      <PersonIcon />
-      <SearchBar
-        show={show}
-        handleShowSearch={handleShowSearch}
-        setShow={setShow}
-        handleFilteredData={props.handleFilteredData}
-      />
-    </div>
-  );
+	return (
+		<div id='navbar'>
+			<Logo show={show} />
+			<div className='navbar-right'>
+				{' '}
+				<SearchBar
+					show={show}
+					handleShowSearch={handleShowSearch}
+					setShow={setShow}
+					handleFilteredData={props.handleFilteredData}
+				/>
+				<PersonIcon />
+			</div>
+		</div>
+	);
 }
