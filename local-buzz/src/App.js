8596d889b1c26b75b1dc421e7afdef8ceb5d1d0f
Mohammad Zafar <mzcoder7@gmail.com>
2023-06-23 11:47:33 +0100

    debugging

--- local-buzz/src/App.js
+++ local-buzz/src/App.js
@@ -1,9 +1,4 @@
-import {
-	BrowserRouter,
-	Route,
-	Routes,
-	useResolvedPath,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context';
 import HomePage from '../src/Components/HomePage/HomePage';
 import LandingPage from '../src/Components/LandingPage/LandingPage';
@@ -20,12 +15,11 @@ import axios from 'axios';
 const REACT_APP_URL = process.env.REACT_APP_URL;
 
 function App() {
-
 	// Defining states for filteredData and events
 	const [filteredData, setFilteredData] = useState([]);
 	const [events, setEvents] = useState([]);
 	const [location, setLocation] = useState(null);
-
+	console.log(location);
 	useEffect(() => {
 		const fetchGeolocation = async () => {
 			try {
@@ -38,7 +32,6 @@ function App() {
 		};
 		fetchGeolocation();
 	}, []);
-
 
 	const fetchData = () => {
 		fetch(`${REACT_APP_URL}events`)
@@ -82,48 +75,46 @@ function App() {
 		}
 	}
 
-
-  return (
-    <BrowserRouter>
-      {/* We are wrapping our whole app with UserProvider so that */}
-      {/* our user is accessible through out the app from any page*/}
-      <UserProvider>
-        <NavBar handleFilteredData={handleFilteredData} />
-        <Routes>
-          <Route
-            exact
-            path='/'
-            element={<LandingPage filteredData={filteredData} />}
-          />
-          <Route exact path='/login' element={<Login />} />
-          <Route exact path='/signup' element={<Signup />} />
-          {/* We are protecting our Home Page from unauthenticated */}
-          {/* users by wrapping it with PrivateRoute here. */}
-          <Route element={<PrivateRoute />}>
-            <Route
-              exact
-              path='/homepage'
-              element={
-                <HomePage
-                  events={events}
-                  // user={user}
-                  // joinEvent={joinEvent}
-                  filteredData={filteredData}
-                />
-              }
-            />
-            <Route
-              exact
-              path='/createeventpage'
-              element={<CreateEventPage addNewEvent={addNewEvent} />}
-            />
-            <Route exact path='/profilePage' element={<ProfilePage />} />
-          </Route>
-        </Routes>
-      </UserProvider>
-    </BrowserRouter>
-  );
-
+	return (
+		<BrowserRouter>
+			{/* We are wrapping our whole app with UserProvider so that */}
+			{/* our user is accessible through out the app from any page*/}
+			<UserProvider>
+				<NavBar handleFilteredData={handleFilteredData} />
+				<Routes>
+					<Route
+						exact
+						path='/'
+						element={<LandingPage filteredData={filteredData} />}
+					/>
+					<Route exact path='/login' element={<Login />} />
+					<Route exact path='/signup' element={<Signup />} />
+					{/* We are protecting our Home Page from unauthenticated */}
+					{/* users by wrapping it with PrivateRoute here. */}
+					<Route element={<PrivateRoute />}>
+						<Route
+							exact
+							path='/homepage'
+							element={
+								<HomePage
+									events={events}
+									// user={user}
+									// joinEvent={joinEvent}
+									filteredData={filteredData}
+								/>
+							}
+						/>
+						<Route
+							exact
+							path='/createeventpage'
+							element={<CreateEventPage addNewEvent={addNewEvent} />}
+						/>
+						<Route exact path='/profilePage' element={<ProfilePage />} />
+					</Route>
+				</Routes>
+			</UserProvider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
