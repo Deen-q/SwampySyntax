bd7d989d53da5830a31af6682f10b4b027af75d5
hroarr28 <thcuk1@hotmail.co.uk>
2023-06-26 12:22:46 +0100

    working on location functions

--- local-buzz/src/App.js
+++ local-buzz/src/App.js
@@ -1,180 +1,160 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { UserProvider } from './contexts/user.context';
-import HomePage from '../src/Components/HomePage/HomePage';
-import LandingPage from '../src/Components/LandingPage/LandingPage';
-import Login from './Pages/Login.Page';
-import PrivateRoute from './Pages/PrivateRoute.Page';
-import Signup from './Pages/SignUp.Page';
-import CreateEventPage from './Components/CreateEventPage/CreateEventPage';
-import ProfilePage from './Components/ProfilePage/profilePage';
-import NavBar from './Components/NavBar/NavBar';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {UserProvider} from "./contexts/user.context";
+import HomePage from "../src/Components/HomePage/HomePage";
+import LandingPage from "../src/Components/LandingPage/LandingPage";
+import Login from "./Pages/Login.Page";
+import PrivateRoute from "./Pages/PrivateRoute.Page";
+import Signup from "./Pages/SignUp.Page";
+import CreateEventPage from "./Components/CreateEventPage/CreateEventPage";
+import ProfilePage from "./Components/ProfilePage/profilePage";
+import NavBar from "./Components/NavBar/NavBar";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {LocationContext} from "./Components/CreateEventPage/AddEventForm/AddEventForm";
+import {useContext} from "react";
 
 // const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
 const REACT_APP_URL = process.env.REACT_APP_URL;
 
 function App() {
-	// Defining states for filteredData and events
-	const [filteredData, setFilteredData] = useState([]);
-	const [events, setEvents] = useState([]);
-	const [location, setLocation] = useState(null);
+  const {latitude, longitude} = useContext(LocationContext);
 
-	console.log(location);
+  console.log(latitude, longitude);
 
-	useEffect(() => {
-		const fetchGeolocation = async () => {
-			try {
-				console.log('fetching geolocation');
-				const response = await axios.get(`${REACT_APP_URL}geolocation`);
-				setLocation(response.data);
-			} catch (error) {
-				console.error('Error fetching geolocation', error);
-			}
-		};
-		fetchGeolocation();
-	}, []);
+  // Defining states for filteredData and events
+  const [filteredData, setFilteredData] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [location, setLocation] = useState(null);
+  const [localEvents, setLocalEvents] = useState([]);
 
-	const fetchData = () => {
-		fetch(`${REACT_APP_URL}events`)
-			.then(response => response.json())
-			.then(data => {
-				setFilteredData(data);
-				setEvents(data);
-			})
-			.catch(error => console.error('Error:', error));
-	};
+  console.log(location);
 
-	useEffect(() => {
-		fetchData();
-	}, []);
+  useEffect(() => {
+    const fetchGeolocation = async () => {
+      try {
+        console.log("fetching geolocation");
+        const response = await axios.get(`${REACT_APP_URL}geolocation`);
+        setLocation(response.data);
+      } catch (error) {
+        console.error("Error fetching geolocation", error);
+      }
+    };
+    fetchGeolocation();
+  }, []);
 
-	/*
-	
-	*/
-	function haversineDistance(location, coords2, isMiles = false) {
-		// Converts degrees to radians
-		function toRad(x) {
-			return (x * Math.PI) / 180;
-		}
+  const fetchData = () => {
+    fetch(`${REACT_APP_URL}events`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFilteredData(data);
+        setEvents(data);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
 
-		const lon1 = location.longitude;
-		const lat1 = location.latitude;
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-		const lon2 = coords2.longitude;
-		const lat2 = coords2.latitude;
+  const eventLocation = {
+    latitude: latitude,
+    longitude: longitude,
+  };
 
-		// Radius of the Earth in kilometers
-		const R = 6371;
+  const userLocation = location;
 
-		// Differences in coordinates
-		const x1 = lat2 - lat1;
-		const dLat = toRad(x1);
-		const x2 = lon2 - lon1;
-		const dLon = toRad(x2);
+  function haversineDistance(isMiles = false) {
+    // Converts degrees to radians
+    function toRad(x) {
+      return (x * Math.PI) / 180;
+    }
 
-		// Haversine formula
-		const a =
-			Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-			Math.cos(toRad(lat1)) *
-				Math.cos(toRad(lat2)) *
-				Math.sin(dLon / 2) *
-				Math.sin(dLon / 2);
-		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		const d = R * c;
+    // Radius of the Earth in kilometers
+    const radius = 6371;
 
-		// Convert to miles if specified
-		if (isMiles) d /= 1.60934;
+    // Differences in coordinates
+    const x1 = eventLocation.latitude - userLocation.latitude;
+    const dLat = toRad(x1);
+    const x2 = eventLocation.longitude - userLocation.longitude;
+    const dLon = toRad(x2);
 
-		return d;
-	}
+    // Haversine formula
+    const haversine =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(toRad(eventLocation.latitude)) *
+        Math.cos(toRad(userLocation.longitude)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(haversine), Math.sqrt(1 - haversine));
+    let diameter = radius * c;
 
-	// Let's say we want to find events within 10 kilometers of the user
-	const maxDistance = 10;
+    // Convert to miles if specified
+    if (isMiles) diameter /= 1.60934;
 
-	// Filter the events based on their distance from the user
-	const nearbyEvents = events.filter(event => {
-		// Create a location object for the event
-		const eventLocation = {
-			latitude: event.latitude,
-			longitude: event.longitude,
-		};
+    return diameter;
+  }
 
-		// Calculate the distance between the user and the event
-		const distance = haversineDistance(userLocation, eventLocation);
+  const maxDistance = 100;
+  const nearbyEvents = events.filter((event) => {
+    const distance = haversineDistance(userLocation, eventLocation);
 
-		// Only include the event in the filtered list if it's within the max distance
-		return distance <= maxDistance;
-	});
+    return distance <= maxDistance;
+  });
+  setLocalEvents(nearbyEvents);
 
-	// Function to handle filtered data
-	function handleFilteredData(event) {
-		const inputValue = event.target.value;
-		const filteredData = events.filter(event =>
-			event.title.toLowerCase().startsWith(inputValue.toLowerCase())
-		);
-		setFilteredData(filteredData);
-	}
-	// this is where we need to make a POST request to the server
-	// to add the new event to the database
-	// and then update the events state with the new event
+  function handleFilteredData(event) {
+    const inputValue = event.target.value;
+    const filteredData = events.filter((event) =>
+      event.title.toLowerCase().startsWith(inputValue.toLowerCase())
+    );
+    setFilteredData(filteredData);
+  }
 
-	async function addNewEvent(newEvent) {
-		console.log('APP.JS LINE 68 ', newEvent);
-		try {
-			const response = await fetch(`${REACT_APP_URL}events`, {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify(newEvent),
-			});
-			const data = await response.json();
-			console.log(data);
-			fetchData();
-		} catch (error) {
-			console.error('Error:', error);
-		}
-	}
+  async function addNewEvent(newEvent) {
+    console.log("APP.JS LINE 68 ", newEvent);
+    try {
+      const response = await fetch(`${REACT_APP_URL}events`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newEvent),
+      });
+      const data = await response.json();
+      console.log(data);
+      fetchData();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
-	return (
-		<BrowserRouter>
-			{/* We are wrapping our whole app with UserProvider so that */}
-			{/* our user is accessible through out the app from any page*/}
-			<UserProvider>
-				<NavBar handleFilteredData={handleFilteredData} />
-				<Routes>
-					<Route
-						exact
-						path='/'
-						element={<LandingPage filteredData={filteredData} />}
-					/>
-					<Route exact path='/login' element={<Login />} />
-					<Route exact path='/signup' element={<Signup />} />
-					{/* We are protecting our Home Page from unauthenticated */}
-					{/* users by wrapping it with PrivateRoute here. */}
-					<Route element={<PrivateRoute />}>
-						<Route
-							exact
-							path='/homepage'
-							element={
-								<HomePage
-									events={events}
-									// user={user}
-									// joinEvent={joinEvent}
-									filteredData={filteredData}
-								/>
-							}
-						/>
-						<Route
-							exact
-							path='/createeventpage'
-							element={<CreateEventPage addNewEvent={addNewEvent} />}
-						/>
-						<Route exact path='/profilePage' element={<ProfilePage />} />
-					</Route>
-				</Routes>
-			</UserProvider>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <UserProvider>
+        <NavBar handleFilteredData={handleFilteredData} />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={<LandingPage filteredData={filteredData} />}
+          />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/signup' element={<Signup />} />
+          <Route element={<PrivateRoute />}>
+            <Route
+              exact
+              path='/homepage'
+              element={<HomePage events={events} filteredData={filteredData} />}
+            />
+            <Route
+              exact
+              path='/createeventpage'
+              element={<CreateEventPage addNewEvent={addNewEvent} />}
+            />
+            <Route exact path='/profilePage' element={<ProfilePage />} />
+          </Route>
+        </Routes>
+      </UserProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- local-buzz/src/Components/CreateEventPage/AddEventForm/AddEventForm.js
+++ local-buzz/src/Components/CreateEventPage/AddEventForm/AddEventForm.js
@@ -1,12 +1,16 @@
 // import { eventData } from "../../Data/EventData";
 import "./AddEventForm.css";
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { readAndCompressImage } from "browser-image-resizer";
+import {useEffect, useState, createContext} from "react";
+import {v4 as uuidv4} from "uuid";
+import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
+import {readAndCompressImage} from "browser-image-resizer";
 
-export default function AddEventForm({ addNewEvent }) {
+export const LocationContext = createContext();
+
+export const LocationProvider = ({children}) => {};
+
+export default function AddEventForm({addNewEvent}) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [date, setDate] = useState("");
@@ -20,38 +24,41 @@ export default function AddEventForm({ addNewEvent }) {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  
+
   const navigate = useNavigate();
-  
+
   function handleSubmit(event) {
     event.preventDefault();
-    const address = firstLineOfAddress + ' ' + city + ' ' + postcode;
-    const urlAddress = address.replace(/ /g, '+');
+    const address = firstLineOfAddress + " " + city + " " + postcode;
+    const urlAddress = address.replace(/ /g, "+");
     console.log(urlAddress);
 
     fetch(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${urlAddress}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
     )
-    .then(response => response.json())
-    .then(data => {
-      
-      if (data && data.results && data.results[0] && data.results[0].geometry) {
-        const latitude2 = data.results[0].geometry.location.lat;
-        const longitude2 = data.results[0].geometry.location.lng;
-        setLatitude(latitude2);
-        console.log (latitude2);
-        setLongitude(longitude2);
-        setIsFormSubmitted(true);
-      } else {
-        throw new Error(`Geocode error: ${data.status}`);
-      }
-    })
-    .catch(error => console.error('Error:', error));
-
+      .then((response) => response.json())
+      .then((data) => {
+        if (
+          data &&
+          data.results &&
+          data.results[0] &&
+          data.results[0].geometry
+        ) {
+          const latitude2 = data.results[0].geometry.location.lat;
+          const longitude2 = data.results[0].geometry.location.lng;
+          setLatitude(latitude2);
+          console.log(latitude2);
+          setLongitude(longitude2);
+          setIsFormSubmitted(true);
+        } else {
+          throw new Error(`Geocode error: ${data.status}`);
+        }
+      })
+      .catch((error) => console.error("Error:", error));
   }
-  
+
   useEffect(() => {
-    if(isFormSubmitted) {
+    if (isFormSubmitted) {
       const newEvent = {
         id: uuidv4(),
         title: title,
@@ -67,20 +74,20 @@ export default function AddEventForm({ addNewEvent }) {
         latitude: latitude,
         longitude: longitude,
       };
-      
+
       addNewEvent(newEvent);
       setIsFormSubmitted(false);
       navigate("/homepage");
     }
   }, [isFormSubmitted]);
-      
+
   const handleImageUpload = async (event) => {
     const file = event.target.files[0];
 
     const config = {
-      quality: 0.5, 
-      maxWidth: 500, 
-      maxHeight: 500, 
+      quality: 0.5,
+      maxWidth: 500,
+      maxHeight: 500,
       autoRotate: true,
       debug: true,
     };
@@ -99,119 +106,120 @@ export default function AddEventForm({ addNewEvent }) {
     }
   };
 
-
   return (
-    <div id="event-form-container">
-      <div className="createEventHeader">
-        <Link to="/homepage" style={{ textDecoration: "none", color: "white" }}>
-          {" "}
-          <button className="closeFormButton"> X </button>
-        </Link>
-      </div>
-      {/*Run the onSubmit once the form has been filled*/}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title</label>
-        <input
-          required
-          type="text"
-          id="title"
-          name="title"
-          placeholder="Add Title"
-          onChange={(event) => setTitle(event.target.value)}
-        ></input>
-        <label htmlFor="description">Description</label>
-        <textarea
-          required
-          type="text"
-          id="description"
-          name="description"
-          placeholder="Add Description"
-          onChange={(event) => setDescription(event.target.value)}
-        ></textarea>
-        <label htmlFor="date">Date</label>
-        <input
-          required
-          type="date"
-          id="date"
-          name="date"
-          placeholder="Date"
-          onChange={(event) => setDate(event.target.value)}
-        ></input>
-        <label htmlFor="time">Time</label>
-        <input
-          required
-          type="time"
-          id="time"
-          name="time"
-          placeholder="Time"
-          onChange={(event) => setTime(event.target.value)}
-        ></input>
-        <label htmlFor="location">Address Line 1</label>
-        <input
-          required
-          type="text"
-          id="firstLineOfAddress"
-          name="firstLineOfAddress"
-          placeholder="First Line Of Address"
-          onChange={(event) => setFirstLineOfAddress(event.target.value)}
-        ></input>
-        <label htmlFor="image">City</label>
-        <input
-          required
-          type="text"
-          id="city"
-          name="city"
-          placeholder="City"
-          onChange={(event) => setCity(event.target.value)}
-        ></input>
-        <label htmlFor="postcode">Postcode</label>
-        <input
-          required
-          type="text"
-          id="postcode"
-          name="postcode"
-          placeholder="Postcode"
-          onChange={(event) => setPostcode(event.target.value)}
-        ></input>
-        <div className="price-capacity">
-          <label htmlFor="price">Price </label>
-          <input
-            className="price"
-            required
-            type="number"
-            id="price"
-            name="price"
-            placeholder="£"
-            onChange={(event) => setPrice(event.target.value)}
-          ></input>
-          <label htmlFor="capacity">Capacity </label>
-          <input
-            className="capacity"
-            required
-            type="number"
-            id="capacity"
-            name="capacity"
-            placeholder="0"
-            onChange={(event) => setCapacity(event.target.value)}
-          ></input>
+    <LocationContext.Provider value={{latitude, longitude}}>
+      <div id='event-form-container'>
+        <div className='createEventHeader'>
+          <Link to='/homepage' style={{textDecoration: "none", color: "white"}}>
+            {" "}
+            <button className='closeFormButton'> X </button>
+          </Link>
         </div>
-        <label htmlFor="image">Image</label>
-        <input
-          type="file"
-          required
-          id="image"
-          name="image"
-          placeholder="Upload your image"
-          onChange={(e) => handleImageUpload(e)}
-        ></input>
-        {/* <label for="tags">Tags:</label> 
+        {/*Run the onSubmit once the form has been filled*/}
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='title'>Title</label>
+          <input
+            required
+            type='text'
+            id='title'
+            name='title'
+            placeholder='Add Title'
+            onChange={(event) => setTitle(event.target.value)}
+          ></input>
+          <label htmlFor='description'>Description</label>
+          <textarea
+            required
+            type='text'
+            id='description'
+            name='description'
+            placeholder='Add Description'
+            onChange={(event) => setDescription(event.target.value)}
+          ></textarea>
+          <label htmlFor='date'>Date</label>
+          <input
+            required
+            type='date'
+            id='date'
+            name='date'
+            placeholder='Date'
+            onChange={(event) => setDate(event.target.value)}
+          ></input>
+          <label htmlFor='time'>Time</label>
+          <input
+            required
+            type='time'
+            id='time'
+            name='time'
+            placeholder='Time'
+            onChange={(event) => setTime(event.target.value)}
+          ></input>
+          <label htmlFor='location'>Address Line 1</label>
+          <input
+            required
+            type='text'
+            id='firstLineOfAddress'
+            name='firstLineOfAddress'
+            placeholder='First Line Of Address'
+            onChange={(event) => setFirstLineOfAddress(event.target.value)}
+          ></input>
+          <label htmlFor='image'>City</label>
+          <input
+            required
+            type='text'
+            id='city'
+            name='city'
+            placeholder='City'
+            onChange={(event) => setCity(event.target.value)}
+          ></input>
+          <label htmlFor='postcode'>Postcode</label>
+          <input
+            required
+            type='text'
+            id='postcode'
+            name='postcode'
+            placeholder='Postcode'
+            onChange={(event) => setPostcode(event.target.value)}
+          ></input>
+          <div className='price-capacity'>
+            <label htmlFor='price'>Price </label>
+            <input
+              className='price'
+              required
+              type='number'
+              id='price'
+              name='price'
+              placeholder='£'
+              onChange={(event) => setPrice(event.target.value)}
+            ></input>
+            <label htmlFor='capacity'>Capacity </label>
+            <input
+              className='capacity'
+              required
+              type='number'
+              id='capacity'
+              name='capacity'
+              placeholder='0'
+              onChange={(event) => setCapacity(event.target.value)}
+            ></input>
+          </div>
+          <label htmlFor='image'>Image</label>
+          <input
+            type='file'
+            required
+            id='image'
+            name='image'
+            placeholder='Upload your image'
+            onChange={(e) => handleImageUpload(e)}
+          ></input>
+          {/* <label for="tags">Tags:</label> 
                 <input type="text" id="tags" name="tags" placeholder="Tags for Event"></input>
                 <label for="link">Link:</label>
                 <input type="text" id="link" name="link" placeholder="Link to Event"></input>
                 */}
-        <input type="submit" value="Submit" id="submit"></input>
-      </form>
-    </div>
+          <input type='submit' value='Submit' id='submit'></input>
+        </form>
+      </div>
+    </LocationContext.Provider>
   );
 }
 
@@ -238,9 +246,9 @@ export default function AddEventForm({ addNewEvent }) {
 //   const [latitude, setLatitude] = useState("");
 //   const [longitude, setLongitude] = useState("");
 //   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  
+
 //   const navigate = useNavigate();
-  
+
 //   function handleSubmit(event) {
 //     event.preventDefault();
 //     const address = firstLineOfAddress + ' ' + city + ' ' + postcode;
@@ -284,20 +292,20 @@ export default function AddEventForm({ addNewEvent }) {
 //         latitude: latitude,
 //         longitude: longitude,
 //       };
-  
+
 //       console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 //       addNewEvent(newEvent);
 //       setIsFormSubmitted(false);
 //     }
 //   }, [isFormSubmitted]);
-      
+
 //   const handleImageUpload = async (event) => {
 //     const file = event.target.files[0];
 
 //     const config = {
-//       quality: 0.5, 
-//       maxWidth: 500, 
-//       maxHeight: 500, 
+//       quality: 0.5,
+//       maxWidth: 500,
+//       maxHeight: 500,
 //       autoRotate: true,
 //       debug: true,
 //     };
