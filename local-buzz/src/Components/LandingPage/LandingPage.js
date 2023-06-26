de61fdb79c422035711350b493d7a15f33bbbe25
UmamahBegg <umamah.begg@gmail.com>
2023-06-26 18:31:25 +0300

    EventCards dont open

--- local-buzz/src/Components/LandingPage/LandingPage.js
+++ local-buzz/src/Components/LandingPage/LandingPage.js
@@ -5,6 +5,9 @@ import "./LandingPage.css";
 export default function LandingPage(props) {
   return (
     <div className='landing-page'>
+    <p className='welcome-member'>
+        Welcome to the Local Buzz! A place where you can meet people in your local area.
+      </p>
       <Link to='/login' style={{textDecoration: "none", color: "white"}}><button>
          Login </button> </Link>
          <Link to='/signup' style={{textDecoration: "none", color: "white"}}> <button>
